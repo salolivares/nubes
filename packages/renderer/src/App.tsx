d1694c005bb42267112989bbfad095e315343b59
Sal Olivares <git@salolivares.com>
2024-07-02 00:53:07 -0700

    use project references

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import { versions } from '#preload';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-
-  console.log(versions);
 
   return (
     <>
