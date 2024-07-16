8c31a10a303cdd90a45c3b470262b769ae73a907
Sal Olivares <git@salolivares.com>
2024-07-15 20:59:13 -0700

    big moves

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,12 +1,27 @@
-import { ThemeProvider } from '@/components/theme-provider';
-import { ModeToggle } from './components/mode-toggle';
-import { LoginForm } from './components/login-form';
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import { ThemeProvider } from './components/theme-provider';
+import { Settings } from './components/Settings';
+import { BaseLayout } from './components/base-layout';
+import { Home } from './components/Home';
+
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<BaseLayout />}>
+      <Route index element={<Home />} />
+      <Route path="settings" element={<Settings />} />
+    </Route>,
+  ),
+);
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="dark">
-      <ModeToggle />
-      <LoginForm />
+    <ThemeProvider>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
