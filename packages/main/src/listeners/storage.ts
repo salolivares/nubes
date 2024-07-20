ebc3201766664707a2d3ae263a333b00346278d7
Sal Olivares <git@salolivares.com>
2024-07-19 23:43:18 -0700

    better typing

--- packages/main/src/listeners/storage.ts
+++ packages/main/src/listeners/storage.ts
@@ -48,16 +48,17 @@ export function addStorageEventListeners() {
   ipcMain.handle(STORAGE_CHANNEL, async (_event, args) => {
     validateArgs(args);
 
-    if (args.action === SECURE_STORAGE_READ || args.action === STORAGE_READ) {
-      const { action, key } = args;
+    const { action, key } = args;
+
+    if (action === SECURE_STORAGE_READ || action === STORAGE_READ) {
       switch (action) {
         case SECURE_STORAGE_READ:
           return Storage.instance.secureRead(key);
         case STORAGE_READ:
           return Storage.instance.read(key);
       }
-    } else if (args.action === SECURE_STORAGE_WRITE || args.action === STORAGE_WRITE) {
-      const { action, key, value } = args;
+    } else if (action === SECURE_STORAGE_WRITE || action === STORAGE_WRITE) {
+      const { value } = args;
       switch (action) {
         case SECURE_STORAGE_WRITE:
           return Storage.instance.secureWrite(key, value);
