f5cfdebd0a0ad530457b9646eb6289a3bde0590c
Sal Olivares <git@salolivares.com>
2024-07-26 00:32:53 -0700

    trpc context

--- src/common/api/trpc.ts
+++ src/common/api/trpc.ts
@@ -1,12 +1,40 @@
 import { initTRPC } from '@trpc/server';
 import superjson from 'superjson';
 import { ZodError } from 'zod';
+import { Storage } from '@/main/drivers/storage';
+import type { CreateContextOptions } from 'electron-trpc/main';
+
+/**
+ * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
+ * it from here.
+ *
+ * Examples of things you may need it for:
+ * - testing, so we don't have to mock Next.js' req/res
+ *
+ */
+const createInnerTRPCContext = () => {
+  return {
+    storage: Storage.instance,
+  };
+};
+
+/**
+ * This is the actual context you will use in your router. It will be used to process every request
+ * that goes through your tRPC endpoint.
+ *
+ * @see https://trpc.io/docs/context
+ */
+export const createTRPCContext = (_opts: CreateContextOptions) => {
+  return createInnerTRPCContext();
+};
+
+export type Context = ReturnType<typeof createTRPCContext>;
 
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
   transformer: superjson,
   errorFormatter({ shape, error }) {
     return {
--- src/main/listeners/trpc.ts
+++ src/main/listeners/trpc.ts
@@ -1,7 +1,8 @@
 import { appRouter } from '@/common/api/root';
+import { createTRPCContext } from '@/common/api/trpc';
 import type { BrowserWindow } from 'electron';
 import { createIPCHandler } from 'electron-trpc/main';
 
 export function addTrpcEventListeners(window: BrowserWindow) {
-  createIPCHandler({ router: appRouter, windows: [window] });
+  createIPCHandler({ router: appRouter, windows: [window], createContext: createTRPCContext });
 }
