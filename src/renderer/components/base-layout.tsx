eb7ded4b26e498a005c757f1442e88a02453a18f
Sal Olivares <git@salolivares.com>
2026-03-22 01:13:15 -0700

    tailwind upgrade

--- src/renderer/components/base-layout.tsx
+++ src/renderer/components/base-layout.tsx
@@ -51,7 +51,7 @@ export function BaseLayout() {
           <ModeToggle />
         </div>
       </header>
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      <main className="flex min-h-[calc(100vh-(--spacing(16)))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
         <Outlet />
       </main>
       <DebugBar />
